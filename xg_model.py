688098a1a358fb78b91caf1da9681ff9fda6500f
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2022-12-28 19:34:55 +0100

    Added functions for logisitic regression

--- xg_model.py
+++ xg_model.py
@@ -3,9 +3,8 @@ import numpy as np
 import os
 from tqdm import tqdm
 from time import sleep
-
-def partition(trainingData):
-    return None
+from random import shuffle, randrange
+import matplotlib.pyplot as plt
 
 # Here I'll make a new function which extracts relevant data and makes new files
 def retrieveData(dirPath):
@@ -38,8 +37,12 @@ def retrieveData(dirPath):
                     curShot.update({'Head' : 0, 'LeftFoot' : 0, 'RightFoot' : 0, 'OtherBodyPart' : 1})
 
                 # Adds end location of shot
-                curShot.update({'YendLoc' : event['shot']['end_location'][1], 
-                'ZendLoc' : event['shot']['end_location'][-1]})
+                curShot.update({'YendLoc' : event['shot']['end_location'][1]})
+                
+                if len(event['shot']['end_location']) == 3:
+                    curShot.update({'ZendLoc' : event['shot']['end_location'][2]})
+                else:
+                    curShot.update({'ZendLoc' : 2.67/2})
                 
                 if event['shot']['outcome']['name'] == 'Goal':
                     curShot.update({'Goal' : 1})
@@ -53,11 +56,19 @@ def retrieveData(dirPath):
             with open(f'shot_data/{filename}_shots.json', "w") as write_file:
                 json.dump(Match, write_file)
 
-
         sleep(0.1)
 
+# Partitions the data set into training and testing data
+def partition(dirPath, testShare):
+    numMatches = len(os.listdir(str(dirPath)))
+    partition = [path for path in os.listdir(str(dirPath))]
+    shuffle(partition)
+    trainData = partition[: round(numMatches * (1 - testShare))]
+    testData = partition[round(numMatches * (1 - testShare)) :]
+    return trainData, testData
+
 # Function for fitting a linear model to training data
-# Currently returns predictions 
+# Currently returns predictions
 def linReg(shotMatrix, isGoal):
     X = shotMatrix
     Xtran = np.transpose(X)
@@ -72,16 +83,103 @@ def linReg(shotMatrix, isGoal):
     print(pred)
     return pred
 
+# Creates matrix of shot data and vector with whether the shot was a goal or not
+def prepData(dirPath, prepData):
+    shotData = []
+    isGoal = []
+    for filename in tqdm(prepData):
+        with open(f'{str(dirPath)}/{filename}', 'r') as rawData:
+            data = json.load(rawData)
+            for shot in data.keys():
+                curShot = []
+                for key in data[shot].keys():
+                    curShot.append(data[shot][key])
+
+                isGoal.append(curShot.pop())
+                shotData.append(curShot)
+    
+    shotData = np.array(shotData)
+    isGoal = np.array(isGoal)
+    isGoal.shape = (isGoal.size, 1)
+
+    return shotData, isGoal
+
+# Fits a logistic regression model to data
+def logReg(trainData, iters, learnRate):
+    shotData, isGoal = prepData('shot_data', trainData)
+    params = np.zeros((np.size(shotData, 1), 1))
+
+    costHist, paramsOpt = gradDescent(shotData, isGoal, 
+    params, learnRate, iters)
+
+    plt.figure()
+    plt.plot(range(len(costHist)), costHist, 'r')
+    plt.title("Convergence Graph of Cost Function")
+    plt.xlabel("Number of Iterations")
+    plt.ylabel("Cost")
+    plt.show()
+
+    print(f'Optimal params are:\n{paramsOpt}')
+    return paramsOpt
+
+# Defines sigmoid function
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def cost(X, y, theta):
+    m = len(y)
+    h = sigmoid(X @ theta)
+    epsilon = 1e-5
+    cost = (1/m)*((((-1) * y).T @ np.log(h + epsilon))-((1-y).T @ np.log(1-h + epsilon)))
+    return cost
+
+def gradDescent(X, y, params, learnRate, iter):
+    m = len(y)
+    costHist = np.zeros((iter, 1))
+
+    for i in tqdm(range(iter)):
+        params = params - (learnRate / m) * np.matmul(np.transpose(X), sigmoid(np.matmul(X, params)) - y)
+        costHist[i] = cost(X, y, params)
+    
+    return (costHist, params)
+
 # Function for testing the model
-def testModel(par, number):
-    filename = os.listdir('data')[number]
-    with open(f'data/{filename}', 'r') as f:
-            data = json.load(f)
+def testModel(params, testData):
+    shotData, isGoal = prepData('shot_data', testData)
+    pred = sigmoid(shotData @ params)
+    avgError = np.mean(abs(pred - isGoal))
+    print(f'Average error: {avgError}')
 
 def main():
-    shotMatrix, isGoal = formatData()
-    pred = linReg(shotMatrix, isGoal)
-    print(np.absolute(np.subtract(pred, isGoal)))
-    print(np.mean(np.absolute(np.subtract(pred, isGoal))))
+    trainData, testData = partition('shot_data', 0.2)
+    params = logReg(trainData, 200, 0.005)
+    testModel(params, testData)
+    return params
 
-# main()
+params = main()
+
+def visualize(params):
+    shotData, isGoal = prepData(
+        'shot_data', os.listdir('shot_data'))
+    xg = sigmoid(shotData @ params)
+    plt.scatter(shotData[:, 0], shotData[:, 1], c = xg, cmap = 'coolwarm')
+    plt.show()
+    return shotData, isGoal, xg
+
+shotData, isGoal, xg = visualize(params)
+
+while True:
+    inp = input('sample [y, g, n]: ')
+    if str(inp) == 'y':
+        n = randrange(0, len(os.listdir('shot_data'))-1)
+        print(shotData[n, :], isGoal[n], xg[n])
+    elif str(inp) == 'g':
+        n = randrange(0, len(os.listdir('shot_data'))-1)
+        while isGoal[n] == 0:
+            n += 1
+        print(shotData[n, :], isGoal[n], xg[n])
+    else:
+        break
+
+n = np.where(xg == max(xg))
+print(shotData[n, :], isGoal[n], xg[n])
