24a0dfbe4ab12abd09b41788c16b2489439cd20a
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2022-12-28 13:56:36 +0100

    Removed old formatData function

--- xg_model.py
+++ xg_model.py
@@ -56,46 +56,6 @@ def retrieveData(dirPath):
 
         sleep(0.1)
 
-
-# Formats relevant data
-def formatData():
-    shotData = []
-    isGoal = []
-
-    # TODO: Make number of matches an input variable
-    for i in tqdm(range(50)):
-        filename = os.listdir('data')[i]
-        with open(f'data/{filename}', 'r') as f:
-            data = json.load(f)
-
-            # TODO: Tidy up. This is pretty ugly... and bib is a bad name
-            for bib in data:
-                if bib['type']['name'] == 'Shot':
-
-                    # Calculate distance to goal
-                    XdistToGoal = min(abs(bib['location'][0]), 
-                    abs(bib['location'][0] - 120))
-                    YdistToGoal = abs(bib['location'][1] - 40)
-
-                    if bib['shot']['body_part']['name'] == 'Head':
-                        shotData.append([XdistToGoal, YdistToGoal, 1, 0, 0])
-                    elif bib['shot']['body_part']['name'] == 'Left Foot':
-                        shotData.append([XdistToGoal, YdistToGoal, 0, 1, 0])
-                    elif bib['shot']['body_part']['name'] == 'Right Foot':
-                        shotData.append([XdistToGoal, YdistToGoal, 0, 0, 1])
-                    else:
-                        shotData.append([XdistToGoal, YdistToGoal, 0, 0, 0])
-
-                    if bib['shot']['outcome']['name'] == 'Goal':
-                        isGoal.append(1)
-                    else:
-                        isGoal.append(0)
-            
-        sleep(2)
-
-    shotMatrix = np.array(shotData)
-    return shotMatrix, isGoal
-
 # Function for fitting a linear model to training data
 # Currently returns predictions 
 def linReg(shotMatrix, isGoal):
