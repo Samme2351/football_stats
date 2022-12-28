1ce737a0637ce90f5c6d569db5c5dc5945b93ab5
Samuel Martinsson <90909426+Samme2351@users.noreply.github.com>
2022-12-28 12:51:40 +0100

    Added new method which extracts shot data adn creates new files

--- xg_model.py
+++ xg_model.py
@@ -4,9 +4,61 @@ import os
 from tqdm import tqdm
 from time import sleep
 
-# Doesn't actually train model, but formats relevant data
-# TODO: This function should be renamed
-def trainModel ():
+def partition(trainingData):
+    return None
+
+# Here I'll make a new function which extracts relevant data and makes new files
+def retrieveData(dirPath):
+    for filename in tqdm(os.listdir(dirPath)):
+        with open(f'{dirPath}/{filename}', 'r') as rawData:
+            data = json.load(rawData)
+            Match = {}
+
+            shots = [event for event in data if event['type']['name'] == 'Shot']
+            matchID = 0
+            for event in shots:
+                shotNum = shots.index(event)
+                curShot = {}
+
+                # Adds data about distance to goal when shot occured
+                curShot.update({'XdistToGoal' : min(abs(event['location'][0]), abs(event['location'][0] - 120)), 
+                'YdistToGoal' : abs(event['location'][1] - 40)})
+
+                # Adds data about body part used
+                if event['shot']['body_part']['name'] == 'Head':
+                    curShot.update({'Head' : 1, 'LeftFoot' : 0, 'RightFoot' : 0, 'OtherBodyPart' : 0})
+
+                elif event['shot']['body_part']['name'] == 'LeftFoot':
+                    curShot.update({'Head' : 0, 'LeftFoot' : 1, 'RightFoot' : 0, 'OtherBodyPart' : 0})
+
+                elif event['shot']['body_part']['name'] == 'RightFoot':
+                    curShot.update({'Head' : 0, 'LeftFoot' : 0, 'RightFoot' : 1, 'OtherBodyPart' : 0})
+
+                else:
+                    curShot.update({'Head' : 0, 'LeftFoot' : 0, 'RightFoot' : 0, 'OtherBodyPart' : 1})
+
+                # Adds end location of shot
+                curShot.update({'YendLoc' : event['shot']['end_location'][1], 
+                'ZendLoc' : event['shot']['end_location'][-1]})
+                
+                if event['shot']['outcome']['name'] == 'Goal':
+                    curShot.update({'Goal' : 1})
+                else:
+                    curShot.update({'Goal' : 0})
+
+                Match.update({shotNum : curShot})
+                matchID += 1
+            
+            # Writes shots of current match to 
+            with open(f'shot_data/{filename}_shots.json', "w") as write_file:
+                json.dump(Match, write_file)
+
+
+        sleep(0.1)
+
+
+# Formats relevant data
+def formatData():
     shotData = []
     isGoal = []
 
@@ -66,8 +118,10 @@ def testModel(par, number):
     with open(f'data/{filename}', 'r') as f:
             data = json.load(f)
 
-# This should be a main function, I'm lazy
-shotMatrix, isGoal = trainModel()
-pred = linReg(shotMatrix, isGoal)
-print(np.absolute(np.subtract(pred, isGoal)))
-print(np.mean(np.absolute(np.subtract(pred, isGoal))))
+def main():
+    shotMatrix, isGoal = formatData()
+    pred = linReg(shotMatrix, isGoal)
+    print(np.absolute(np.subtract(pred, isGoal)))
+    print(np.mean(np.absolute(np.subtract(pred, isGoal))))
+
+# main()
